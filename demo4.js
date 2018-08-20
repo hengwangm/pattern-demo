ed61c4642075cc8a4ac317cc1e6e9c322652a8d2
hengwangm <371535926@qq.com>
2018-08-20 23:36:06 +0800

    选择排序，节流

--- demo4.js
+++ demo4.js
@@ -1,4 +1,40 @@
 /**
  * 发布/订阅模式
  * 观察者模式。
+ * 一个简单实现版本，复杂的需要events可以多次on一个事件，需要push
  */
+
+ class Event{
+
+    constructor(){
+        this.events = {};
+    }
+
+    on(eventName,fn){
+        this.events[eventName] = fn;
+    }
+
+    emit(eventName ,data){
+        if(this.events[eventName]){
+            var fn = this.events[eventName];
+            fn.call(this,data)
+        }
+    }
+
+    remove(eventName){
+        if(this.events[eventName]){
+            this.events[eventName] = null;
+        }
+    }
+ }
+
+ var event = new Event();
+ event.on('aaaa',function(event,data){
+    console.log(event)
+ })
+ event.on('bbbb',function(event,data){
+    console.log(event)
+ })
+ event.remove('aaaa')
+ event.emit('aaaa',1)
+ event.emit('bbbb',2)
